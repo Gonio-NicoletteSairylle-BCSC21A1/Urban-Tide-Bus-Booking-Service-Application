42696534beb72a8b3cc1bd54dca7d71391a9cac7
Gonio-NicoletteSairylle-BCSC21A1 <nsairyllegonio@gmail.com>
2024-10-31 19:09:11 +0800

    Add project files.

---
+++ Urban Tide Bus Booking Service Application/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urban_Tide_Bus_Booking_Service_Application
+{
+    public partial class Form2MainMenu : Form
+    {
+        public Form2MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1book_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2see_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3routes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4schedules_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
